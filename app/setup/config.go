e80cf5057d7b42f66adf6303a53cb9846fb7f110
Oyewole S. Abayomi <oyewoleabayomi@gmail.com>
2022-08-16 19:44:33 +0100

    refactor: rename and update gitignore file

--- app/setup/config.go
+++ app/setup/config.go
@@ -13,24 +13,29 @@ type Config struct {
 }
 
 func GetOptions() []fx.Option {
-	return SetupApp(Config{
+	return App(Config{
 		ServiceName: "social-login",
 		CusTomFxOptions: []fx.Option{
-			// add your controller/service here <-
+			// controller/service goes here <-
 			fx.Provide(),
 			fx.Invoke(router.SetupRoutes),
 		},
 	})
 }
 
-func SetupApp(config Config) []fx.Option {
+func App(config Config) []fx.Option {
 	options := []fx.Option{
 		fx.Provide(NewZapLogger),
-		fx.Provide(func(logger *zap.Logger) *gin.Engine { return gin.New() }),
+		fx.Provide(SetupGin),
 	}
 
 	options = append(options, config.CusTomFxOptions...)
 	return options
+}
+
+// SetupGin - initialize gin framework
+func SetupGin(logger *zap.Logger) *gin.Engine {
+	return gin.New()
 }
 
 // NewZapLogger -logger
